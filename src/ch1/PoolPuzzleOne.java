98819a07f218bfb66610b21914fe5a914ec52356
Kosinfernal <94693119+Kosinfernal@users.noreply.github.com>
2022-04-21 00:57:55 +0300

    changes

---
+++ src/ch1/PoolPuzzleOne.java
@@ -0,0 +1,45 @@
+package ch1;
+/*what we have/left
+        x > 0
+        x > 3;
+        x -=1;
+        x -=2;
+        System.out.print("an");//definition
+        System.out.print("annoys");//definition
+ */
+
+/*what we need:
+ * a noise
+ * annoys
+ * an oyster
+ */
+public class PoolPuzzleOne {
+    public static void main(String[] args) {
+
+        int x = 0;//static
+
+        while (x < 4) {//definition for while
+
+            System.out.print("a");//definition
+
+            if (x < 1) {//static
+                System.out.print(" ");//definition
+            }
+
+            System.out.print("n");//definition
+
+            if (x > 1) {//definition
+                System.out.print(" oyster");//definition
+                x += 2;//definition
+            }
+            if (x == 1) {//static
+                System.out.print("noys");//definition
+            }
+            if (x < 1) {//definition
+                System.out.print("oise");//definition
+            }
+            System.out.println();//static
+            x++;//definition
+        }
+    }
+}
